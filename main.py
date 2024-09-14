3247094174c01e5f387f2f4f8fb12fccbb111292
Mohammed Ismail <145321352+mohammedismailmi@users.noreply.github.com>
2024-09-14 14:23:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,3 @@ def disconnect():
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
 
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
